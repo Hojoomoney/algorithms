27d23f1abe83ecd88187a409bc72a41df2ed4291
Hojoomoney <kimhojoo51@gmail.com>
2024-03-08 20:17:40 +0900

    password fixed

--- algorithmTest/Password.java
+++ algorithmTest/Password.java
@@ -1,45 +1,62 @@
 package algorithmTest;
 
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Password {
-    public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<String> passwords = new ArrayList<>();
         String password = "";
         while(!password.equals("end")){
-            password = scan.next();
+            password = br.readLine();
             if(!password.equals("end")){
                 passwords.add(password);
             }
         }
-
-
-        
+        for (String word : passwords) {
+            if(validation(word)){
+                System.out.println("<" + word + "> is acceptable.");
+            } else {
+                System.out.println("<" + word + "> is not acceptable.");
+            }
+        }
     }
-    static boolean checkVowels(String word){
-        if(word.contains("a") ||
-                word.contains("e") ||
-                word.contains("i") ||
-                word.contains("o") ||
-                word.contains("u")){
-            return true;
-        } else return false;
-    }
-    static boolean check2inRow(char first, char second){
-        if(first == second){
-            if(first == 'e' || first == 'o'){
-                return true;
-            } else return false;
-        } else return true;
-    }
-    static boolean checkConOrVowInRow(String first, String second, String third){
-        if(checkVowels(first) && checkVowels(second) && checkVowels(third)){
+    static boolean validation(String word){
+        if(!word.contains("a") &&
+                !word.contains("e") &&
+                !word.contains("i") &&
+                !word.contains("o") &&
+                !word.contains("u")){
             return false;
         }
-        if(!checkVowels(first) && !checkVowels(second) && !checkVowels(third)){
-            return false;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] == arr[i+1]) {
+                if (arr[i] != 'e' && arr[i] != 'o') {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length-2; i++) {
+            if (checkVowels(arr[i]) && checkVowels(arr[i+1]) && checkVowels(arr[i+2])) {
+                return false;
+            }
+            if (!checkVowels(arr[i]) && !checkVowels(arr[i+1]) && !checkVowels(arr[i+2])) {
+                return false;
+            }
         }
         return true;
+    }
+    static boolean checkVowels(char alphabet){
+        if(alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
