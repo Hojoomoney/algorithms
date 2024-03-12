c00c84487fd0b45cfed4c6809e4596f12d4b1faa
Hojoomoney <kimhojoo51@gmail.com>
2024-03-13 06:58:13 +0900

    algorithm test

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -7,12 +7,6 @@ import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        //1152번 단어 개수 세기
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String sentence = br.readLine();
-        //StringTokenizer는 디폴트로 공백기준으로 구분해줌
-        StringTokenizer st = new StringTokenizer(sentence);
-        //countTokens 문자개수를 바로 리턴해준다.
-        System.out.println(st.countTokens());
+        System.out.println("Hello World!");
     }
 }
