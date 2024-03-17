0beddd5f02092256b657edab1a7643259a7be3dd
Hojoomoney <kimhojoo51@gmail.com>
2024-03-17 20:02:49 +0900

    1550 algorithm practic decode

--- algorithmTest/Test2.java
+++ algorithmTest/Test2.java
@@ -1,2 +1,13 @@
-package algorithmTest;public class Test2 {
+package algorithmTest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String sixteen = br.readLine();
+        int ten = Integer.parseInt(sixteen,16);
+        System.out.println(ten);
+    }
 }
