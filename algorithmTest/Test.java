d9ef4b9226d2367a776a69698979db3930101908
Hojoomoney <kimhojoo51@gmail.com>
2024-03-23 06:56:26 +0900

    git push problem test

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        //1152 단어 개수 세기
+        //1152번 단어 개수 세기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String sentence = br.readLine();
         //StringTokenizer는 디폴트로 공백기준으로 구분해줌
