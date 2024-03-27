984d66e695b718ed1c82aaf2e06ad4c9b1eacc8d
Hojoomoney <kimhojoo51@gmail.com>
2024-03-27 20:35:54 +0900

    easy problem to solve practice

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -3,25 +3,28 @@ package algorithmTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int numTest = Integer.parseInt(br.readLine());
-        int[] results = new int[numTest];
-        for (int i = 0; i < numTest; i++) {
-            int[] numbers = new int[10];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 10; j++) {
-                numbers[j] = Integer.parseInt(st.nextToken());
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       List<Integer> list = new ArrayList<>(1000);
+        int count = 1;
+        while (list.size() < 1000){
+            for (int i = 0; i < count; i++) {
+                    list.add(count);
             }
-            Arrays.sort(numbers);
-            results[i] = numbers[7];
+            count++;
         }
-        for (int i = 0; i < numTest; i++) {
-            System.out.println(results[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        for (int i = num1-1; i < num2; i++) {
+            sum += list.get(i);
         }
+        System.out.println(sum);
     }
 }
