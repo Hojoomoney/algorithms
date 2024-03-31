3adc900e3d9aeac501386e54f7017fecb92a1a83
Hojoomoney <kimhojoo51@gmail.com>
2024-03-31 20:31:30 +0900

    fix : error pulling

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -26,3 +26,24 @@ public class Test {
         System.out.println(sb);
     }
 }
+=======
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       List<Integer> list = new ArrayList<>(1000);
+        int count = 1;
+        while (list.size() < 1000){
+            for (int i = 0; i < count; i++) {
+                    list.add(count);
+            }
+            count++;
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        for (int i = num1-1; i < num2; i++) {
+            sum += list.get(i);
+        }
+        System.out.println(sum);
+    }
+}
+>>>>>>> 984d66e695b718ed1c82aaf2e06ad4c9b1eacc8d
