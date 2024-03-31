c6ec5d88046e387c413c7e5aac7cf2f3ea35066e
Hojoomoney <kimhojoo51@gmail.com>
2024-03-31 20:28:30 +0900

    algorithm sugar 2839 practice

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -3,17 +3,26 @@ package algorithmTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
-        System.out.println(fibonacci(number));
-    }
-    static int fibonacci(int n){
-        if (n==0) return 0;
-        if (n==1) return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<String, String> mine = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            mine.put(st.nextToken(),"0");
+        }
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if(mine.get(st.nextToken()) != null) sb.append("1 ");
+            else sb.append("0 ");
+        }
+        System.out.println(sb);
     }
 }
