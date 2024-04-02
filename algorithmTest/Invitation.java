773e6c10a9872f212566d6d8aac497744741e5a8
Hojoomoney <kimhojoo51@gmail.com>
2024-04-02 21:40:03 +0900

    invitaion practice, and 1037

--- algorithmTest/Invitation.java
+++ algorithmTest/Invitation.java
@@ -4,19 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Test {
+public class Invitation {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int max = 0;
+        int count = 2;
+        Integer[] arr = new Integer[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0] * arr[n-1]);
-
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max,Integer.parseInt(st.nextToken()) + count);
+            count++;
+        }
+        System.out.println(max);
     }
 }
