02732134dfc6347318edc3773733c68890942119
HojooKim <138412343+Hojoomoney@users.noreply.github.com>
2024-04-10 22:08:20 +0900

    Update Test.java

--- algorithmTest/Test.java
+++ algorithmTest/Test.java
@@ -1,22 +1,35 @@
-package algorithmTest;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        List<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == -1) break;
+            numbers.add(n);
         }
-        Arrays.sort(arr);
-        System.out.println(arr[0] * arr[n-1]);
+        for (Integer number : numbers) {
+            StringBuilder sb = new StringBuilder();
+            int sum = 0;
+            for (int i = 1; i <= number/2; i++) {
+                if (number % i == 0) {
+                    sb.append(i).append(" + ");
+                    sum += i;
+                }
+            }
+            sb.delete(sb.length() - 2, sb.length());
+            if (sum == number) {
+                System.out.println(number + " = " + sb);
+            } else {
+                System.out.println(number + " is NOT perfect.");
+            }
+        }
 
     }
 }
