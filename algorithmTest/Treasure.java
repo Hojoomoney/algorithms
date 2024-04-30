af8aee1f29866c307722e2ebf97ca54c11838940
Hojoomoney <kimhojoo51@gmail.com>
2024-04-30 19:18:47 +0900

    Treasure(1026) practice

---
+++ algorithmTest/Treasure.java
@@ -0,0 +1,36 @@
+package algorithmTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Treasure {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A, Comparator.reverseOrder());
+        Arrays.sort(B);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[i];
+        }
+        System.out.println(sum);
+
+    }
+}
