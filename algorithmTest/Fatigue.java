5c2cf717e3107d446a7c18f269ebbfc5efbe9a35
Hojoomoney <kimhojoo51@gmail.com>
2024-05-04 13:42:03 +0900

    Fatigue(22864) practice

---
+++ algorithmTest/Fatigue.java
@@ -0,0 +1,29 @@
+package algorithmTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Fatigue{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int count = 0, p = 0, answer = 0;
+        while(count != 24) {
+            if(p + a <= m) {
+                answer += b;
+                p += a;
+            }
+            else {
+                p = Math.max(p - c, 0);
+            }
+            count++;
+        }
+        System.out.print(answer);
+    }
+}
