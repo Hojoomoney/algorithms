29b069bf1b8d5d7fb14ab9a31e54603302cd5804
Hojoomoney <kimhojoo51@gmail.com>
2024-06-11 15:03:43 +0900

    Tteokguk(20937) practice fix

--- algorithmTest/Tteokguk.java
+++ algorithmTest/Tteokguk.java
@@ -3,23 +3,30 @@ package algorithmTest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Tteokguk {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> bowls = new ArrayList<>();
+        int[] bowls = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            bowls.add(Integer.parseInt(st.nextToken()));
+            bowls[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(bowls);
+        int result = 1;
         int count = 1;
-        HashSet<Integer> set = new HashSet<>(bowls);
-        for (Integer i : set) {
-            count = Math.max(Collections.frequency(bowls, i),count);
+        for (int i = 1; i < n; i++) {
+            if (bowls[i] == bowls[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            result = Math.max(result, count);
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
